7b44fbd7a1adbde91eeb7af50402243129f2be11
akiya <akiya0524@gmail.com>
2021-07-15 23:28:43 -0700

    Initial Commit

---
+++ RxSwiftExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RxSwiftExample
+//
+//  Created by Akiya Ozawa on R 3/07/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RxSwiftExample/RxSwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwiftExampleApp.swift
+//  RxSwiftExample
+//
+//  Created by Akiya Ozawa on R 3/07/15.
+//
+
+import SwiftUI
+
+@main
+struct RxSwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
